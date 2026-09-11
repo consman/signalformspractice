2ce03fe147c62cdae47f0e28ef05a9af801cb5c6
bob <bob.dev.oak@gmail.com>
2026-09-11 16:21:11 -0700

    remove httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners,provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { AbsOrderService } from './abs-order-service';
 import { environment } from '../environments/environment';
 import { orderServiceFactory } from './orderServiceFactory';
@@ -10,8 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(), 
-    {provide: 'http', useValue: HttpClient}, 
     {provide: AbsOrderService, useFactory:(isProd: boolean)=> orderServiceFactory(isProd), deps: ['IS_PROD_ENVIRONMENT']},
     {provide: 'IS_PROD_ENVIRONMENT', useValue: environment.production}
   ]
